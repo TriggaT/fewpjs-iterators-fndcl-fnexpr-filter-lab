9355ea00d96c4af47c72477a90f45aa09824b2de
Trayshawn <traytwebb14@yahoo.com>
2021-02-25 20:00:42 -0600

    commit tree eae1f5af5961632320bd600d6b8156b42b06dda3

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+function findMatching(drivers, driver){
+    return drivers.filter(d => d.toLowerCase().includes(driver.toLowerCase()) )
+
+}
+
+function fuzzyMatch(drivers, fuzz){
+    return drivers.filter(d => d.toLowerCase().startsWith(fuzz.toLowerCase()) )
+
+}
+
+function matchName(drivers, driver){
+    return drivers.filter(d => d.name.toLowerCase().includes(driver.toLowerCase()))
+
+}
